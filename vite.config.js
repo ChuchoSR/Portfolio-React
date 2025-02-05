c5554fd00bc1dff521054331b5ee3cccdd2b5df8
Chucho <jesusjaviersierrarondon@gmail.com>
2025-02-05 00:41:22 -0300

    Configuración base relativa en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/react-portfolio/',
+  plugins: [react()],
+  base: "./" // Base relativa para evitar problemas en rutas de archivos
 })
